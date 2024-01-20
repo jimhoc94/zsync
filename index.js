b75f5acd9185c0127a0526b21adc0ceabdae06e9
jimhoc94 <73989467+jimhoc94@users.noreply.github.com>
2024-01-20 12:54:02 +0100

    api call

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const axios = require("axios");
+var fs = require("fs");
+
 const main = async () => {
   let numberCall = 0;
   try {
@@ -8,6 +11,33 @@ const main = async () => {
     } else {
       numberCall = process.argv[2];
       console.log("Number of API calls : " + numberCall);
+    }
+
+    for (let i = 1; i <= numberCall; i++) {
+      axios
+        .get("https://randomuser.me/api/?results=1")
+        .then(function (response) {
+          // handle success
+          let first = response.data.results[0].name.first;
+          let last = response.data.results[0].name.last;
+          let title = response.data.results[0].name.title;
+          let data = response.data.results[0];
+          let dir = "./output/user";
+
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+          }
+          let fileName = dir + "/" + first + "_" + last + ".json";
+          console.log(`Writting, ${fileName}`);
+          fs.writeFileSync(fileName, JSON.stringify(data, null, 2));
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        });
     }
   } catch (error) {
     console.log(error.message);
