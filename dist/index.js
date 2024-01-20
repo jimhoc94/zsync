4aac42e8b18b044a139b2df173f1165d458583b5
jimhoc94 <73989467+jimhoc94@users.noreply.github.com>
2024-01-20 12:03:27 +0100

    modif .gitignore

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+const main = async () => {
+  try {
+    console.log("Hello World from zsync !");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Call the main function to run the action
+main();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
